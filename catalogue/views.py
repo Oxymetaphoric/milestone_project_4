f46b6f2264276844457eb39f09ecdc8d35897378
Paul Tapping <zombieplayground@gmail.com>
2024-10-26 14:34:20 +0100

    BUILD: initial catalogue view

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import StockItem 
 
 # Create your views here.
+
+def display_stock_items(request):
+    """
+    A view to display, search, and sort catalogue items
+    """
+    catalogue = StockItem.objects.all()
+
+    context = {
+            'catalogue': catalogue,
+            }
+
+    return render(request, 'catalogue/catalogue.html', context)
